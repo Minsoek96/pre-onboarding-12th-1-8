7ab125c9d8e254a7d52b98a41b676395afea3a61
이종혁 <dlwhd5717@naver.com>
2023-08-24 12:17:28 +0900

    url 및 토큰 변수 설정

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,4 +4,4 @@ export const API_ROUTES = {
     TODOS: "/todos",
   };
   export const BASE_URL = "https://www.pre-onboarding-selection-task.shop/";
-  export const ACCESS_TOKEN_KEY = "access_token";
+  export const ACCESS_TOKEN_KEY = "access_token"
