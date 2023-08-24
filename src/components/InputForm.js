20f3fbfe05df5a1ba271ed304389cf47e20e3a26
이종혁 <dlwhd5717@naver.com>
2023-08-24 15:55:02 +0900

    useInput hook을 사용한 form 기능 구현

---
+++ src/components/InputForm.js
@@ -0,0 +1,53 @@
+import Button from "./Button";
+import { useInput } from '../hooks/useInput';
+const emailValidate = (target) => target.includes("@");
+const pwValidate = (target) => target.trim().length >= 8;
+const InputForm = ({ buttonContent, buttonId, onSuccessSubmitEvent }) => {
+  const {
+    value: emailValue,
+    onChange: onChangeEmailInput,
+    setIsFocused: setEmailIsFocused,
+    invalid: emailIsInValid,
+  } = useInput("", emailValidate);
+  const {
+    value: pwValue,
+    onChange: onChangePwInput,
+    setIsFocused: setPwIsFocused,
+    invalid: pwIsInValid,
+  } = useInput("", pwValidate);
+  const submitEventHandler = (e) => {
+    e.preventDefault();
+    setPwIsFocused(true);
+    if (emailIsInValid || pwIsInValid) {
+      //실패했을 때
+      return;
+    }
+    onSuccessSubmitEvent({ email: emailValue, password: pwValue });
+  };
+  console.log(emailIsInValid,pwIsInValid)
+  return (
+    <form onSubmit={submitEventHandler}>
+      <input
+        data-testid="email-input"
+        type="text"
+        value={emailValue}
+        onChange={onChangeEmailInput}
+        onBlur={() => setEmailIsFocused(true)}></input>
+      {emailIsInValid && <em>이메일을 확인하세요</em>}
+      <input
+        data-testid="password-input"
+        type="password"
+        value={pwValue}
+        onChange={onChangePwInput}
+        onBlur={() => setPwIsFocused(true)}></input>
+      {pwIsInValid && <em>비밀번호를 확인하세요</em>}
+      <Button
+        content={buttonContent}
+        disabled={!emailValidate(emailValue) || !pwValidate(pwValue)}
+        data-testid={buttonId}
+      />
+    </form>
+  );
+};
+export default InputForm;
+
